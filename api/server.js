7449fe38a7ee522683e50c1de551073a5dab1211
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-05-02 10:48:38 -0400

    chore(package.json): add cors to dependencies

--- api/server.js
+++ api/server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3005;
 const envelopesRouter = require("./routes/envelopes");
+const cors = require("cors");
 
 app.use(express.json()); //Middleware to parse incoming requests
-
+app.use(cors());
 // Registers routes for /envelopes
 app.use("/envelopes", envelopesRouter);
 
