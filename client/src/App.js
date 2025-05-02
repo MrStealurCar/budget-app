04f697aa3ea1a355e89b45684ec16d7d3d4eaae3
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-05-02 11:35:31 -0400

    refactor(client/app): update state variable name and update import path for BudgetCard

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
-
+import React, { useState } from "react";
+import BudgetCard from "./components/BudgetCard/BudgetCard";
 function App() {
-  const [budget, setBudget] = useState([]);
+  const [entry, setEntry] = useState([]);
   const [title, setTitle] = useState("");
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Budget Planner</h1>
       </header>
+      <main>
+        <BudgetCard entry={entry} setEntry={setEntry} />
+      </main>
     </div>
   );
 }
