300311027af396f3ffc6737c91d2720615dc8bc7
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-05-07 09:55:52 -0400

    feat(BudgetCard): import event handlers and add logic for editing entries

--- client/src/components/BudgetCard/BudgetCard.js
+++ client/src/components/BudgetCard/BudgetCard.js
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from "react";
 import fetchEnvelopes from "../../api/api";
 import "./BudgetCard.css";
+import {
+  handleEdit,
+  handleDelete,
+  handleTransfer,
+} from "../../api/budgetActions";
 function BudgetCard({ entry, setEntry }) {
   const [editId, setEditId] = useState(null);
+  const [newTitle, setNewTitle] = useState("");
+  const [newBudget, setNewBudget] = useState(0);
   useEffect(() => {
     const getEnvelopes = async () => {
       const data = await fetchEnvelopes();
@@ -20,10 +27,42 @@ function BudgetCard({ entry, setEntry }) {
           <li key={item.id} className="data">
             {item.title}: ${item.budget}
             <div className="handler-buttons">
-              <button title="Delete entry">❌</button> {/* deletes entry */}
-              <button title="Edit entry">✏️</button>
-              <button title="Transfer funds">🔁</button>
-              {/* transfers funds between entries */}
+              <button title="Delete entry">❌ {/* deletes entry */}</button>
+              <button
+                title="Edit entry"
+                onClick={() => {
+                  setEditId(item.id);
+                  setNewTitle(item.title);
+                  setNewBudget(item.budget);
+                }}
+              >
+                ✏️ {/* edit entry */}
+              </button>
+              {editId === item.id && (
+                <div>
+                  <input
+                    value={newTitle}
+                    onChange={(e) => setNewTitle(e.target.value)}
+                  />
+                  <input
+                    value={newBudget}
+                    onChange={(e) => setNewBudget(Number(e.target.value))}
+                  />
+                  <button
+                    onClick={() => {
+                      handleEdit(item.id, newTitle, newBudget, setEntry);
+                      setEditId(null);
+                      setNewTitle("");
+                      setNewBudget(0);
+                    }}
+                  >
+                    Save
+                  </button>
+                </div>
+              )}
+              <button title="Transfer funds">
+                🔁 {/* transfers funds between entries */}
+              </button>
             </div>
           </li>
         ))}
