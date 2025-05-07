13b958eb95854c70a25de9985383eecaae99b3fc
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-05-07 10:40:34 -0400

    refactor(EditMode): move code from BudgetCard responsible for  editing entry into its own component

---
+++ client/src/components/EditMode/EditMode.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { handleEdit } from "../../api/budgetActions";
+import "./EditMode.css";
+function EditMode({
+  item,
+  newTitle,
+  newBudget,
+  setEntry,
+  setNewTitle,
+  setNewBudget,
+  setEditId,
+}) {
+  return (
+    <div>
+      <input
+        className="input"
+        value={newTitle}
+        onChange={(e) => setNewTitle(e.target.value)}
+      />
+
+      <input
+        className="input"
+        value={newBudget}
+        onChange={(e) => setNewBudget(Number(e.target.value))}
+      />
+      <button
+        onClick={() => {
+          handleEdit(item.id, newTitle, newBudget, setEntry);
+          setEditId(null);
+          setNewTitle("");
+          setNewBudget(0);
+        }}
+      >
+        Save
+      </button>
+    </div>
+  );
+}
+
+export default EditMode;
