f62910da519c00b88e9f204c03bb5aa8d1ab2e83
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-05-08 12:36:00 -0400

    feat(AddEntry): add conditional rendering for input fields

--- client/src/components/AddEntry/AddEntry.js
+++ client/src/components/AddEntry/AddEntry.js
@@ -4,27 +4,36 @@ import { handleCreate } from "../../api/budgetActions";
 function AddEntry({ setEntry }) {
   const [title, setTitle] = useState("");
   const [budget, setBudget] = useState(0);
-
+  const [isVisible, setIsVisible] = useState(false);
   return (
     <div>
-      <input
-        value={title}
-        placeholder="Budget name"
-        type="text"
-        onChange={(e) => setTitle(e.target.value)}
-        required
-      />
-      <input
-        value={budget}
-        type="number"
-        min={0}
-        onChange={(e) => setBudget(Number(e.target.value))}
-        required
-      />
-      <button onClick={() => handleCreate(title, budget, setEntry)}>
-        Save
+      <button onClick={() => setIsVisible((prevState) => !prevState)}>
+        Add Entry
       </button>
-      <button>Cancel</button>
+      {isVisible && (
+        <div>
+          <input
+            value={title}
+            placeholder="Budget name"
+            type="text"
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+          <input
+            value={budget}
+            type="number"
+            min={0}
+            onChange={(e) => setBudget(Number(e.target.value))}
+            required
+          />
+          <button onClick={() => handleCreate(title, budget, setEntry)}>
+            Save
+          </button>
+          <button onClick={() => setIsVisible((prevState) => !prevState)}>
+            Cancel
+          </button>
+        </div>
+      )}
     </div>
   );
 }
