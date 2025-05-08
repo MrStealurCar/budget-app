4d0739cb5ee918308f10e59a7b366fe9dc59a1f2
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-05-08 12:15:18 -0400

    feat(AddEntry): hook up input fields with inline callbacks and import handleCreate

--- client/src/components/AddEntry/AddEntry.js
+++ client/src/components/AddEntry/AddEntry.js
@@ -1,12 +1,28 @@
 import React, { useState } from "react";
 import "./AddEntry.css";
-
+import { handleCreate } from "../../api/budgetActions";
 function AddEntry() {
   const [title, setTitle] = useState("");
   const [budget, setBudget] = useState(0);
+
   return (
     <div>
-      <input />
+      <input
+        value={title}
+        placeholder="Budget name"
+        type="text"
+        onChange={(e) => setTitle(e.target.value)}
+        required
+      />
+      <input
+        value={budget}
+        type="number"
+        min={0}
+        onChange={(e) => setBudget(Number(e.target.value))}
+        required
+      />
+      <button onClick={handleCreate}>Save</button>
+      <button>Cancel</button>
     </div>
   );
 }
