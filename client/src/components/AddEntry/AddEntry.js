b79cdd2faf9db9cc4039aa8e70d996f1f994dd57
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-05-08 11:13:06 -0400

    feat(AddEntry): create AddEntry component and add state variables

---
+++ client/src/components/AddEntry/AddEntry.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import "./AddEntry.css";
+
+function AddEntry() {
+  const [title, setTitle] = useState("");
+  const [budget, setBudget] = useState(0);
+  return (
+    <div>
+      <input />
+    </div>
+  );
+}
+
+export default AddEntry;
