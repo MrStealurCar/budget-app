2768ff68c4d52a7d6479025de385fd6355e4db36
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-05-09 12:24:30 -0400

    feat(DetailedView): move action buttons into DetailedView component, pass needed props

--- client/src/components/DetailedView/DetailedView.js
+++ client/src/components/DetailedView/DetailedView.js
@@ -1,6 +1,14 @@
 import React from "react";
 import "./DetailedView.css";
-function DetailedView({ item, setViewId }) {
+import ActionButtons from "../ActionButtons/ActionButtons";
+function DetailedView({
+  item,
+  setViewId,
+  setEditId,
+  setNewTitle,
+  setNewBudget,
+  setEntry,
+}) {
   return (
     <div>
       <span className="entry-details">
@@ -8,14 +16,23 @@ function DetailedView({ item, setViewId }) {
         <span>Title: {item.title},</span>
         <span>Budget: ${item.budget}</span>
       </span>
-      <button
-        className="close-button"
-        onClick={() => {
-          setViewId(null);
-        }}
-      >
-        Close
-      </button>
+      <div className="action-buttons">
+        <ActionButtons
+          setEditId={setEditId}
+          setNewTitle={setNewTitle}
+          setNewBudget={setNewBudget}
+          item={item}
+          setEntry={setEntry}
+        />
+        <button
+          className="close-button"
+          onClick={() => {
+            setViewId(null);
+          }}
+        >
+          Close
+        </button>
+      </div>
     </div>
   );
 }
