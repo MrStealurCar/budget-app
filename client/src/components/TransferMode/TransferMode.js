71d29a4e509fd2b82e1d93c17b98859e2e21c393
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-05-13 12:31:04 -0400

    feat(TransferMode): add code responsible for transferring funds to different entries

--- client/src/components/TransferMode/TransferMode.js
+++ client/src/components/TransferMode/TransferMode.js
@@ -1,5 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import "./TransferMode.css";
+import { handleTransfer } from "../../api/budgetActions";
+function TransferMode({ sourceId, entry, setEntry }) {
+  const [transferAmount, setTransferAmount] = useState(0);
+  const [destination, setDestination] = useState(null);
 
-function TransferMode() {}
+  return (
+    <div className="test">
+      <input
+        value={transferAmount}
+        placeholder="Amount"
+        type="number"
+        onChange={(e) => setTransferAmount(Number(e.target.value))}
+        required
+      />
+      <select onClick={(e) => setDestination(Number(e.target.value))} required>
+        {entry
+          .filter((item) => item.id !== sourceId)
+          .map((item) => (
+            <option key={item.id} value={item.id}>
+              {item.title}
+            </option>
+          ))}
+      </select>
+      <button
+        onClick={() => {
+          handleTransfer(sourceId, destination, transferAmount, setEntry);
+        }}
+      >
+        Transfer
+      </button>
+    </div>
+  );
+}
 
 export default TransferMode;
