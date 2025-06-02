e7b94b9d3962f6ceaae29c9b1bd5308845cd0d6d
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-06-02 11:02:44 -0400

    feat(App): allow user to set budget and display on screen

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,43 @@ import BudgetCard from "./components/BudgetCard/BudgetCard";
 import AddEntry from "./components/AddEntry/AddEntry";
 function App() {
   const [entry, setEntry] = useState([]);
-
+  const [totalBudget, setTotalBudget] = useState(0);
+  const [savedTotal, setSavedTotal] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <h1>Budget Planner</h1>
+        <div className="budget-field">
+          <input
+            className="new-budget"
+            placeholder="Monthly Budget"
+            type="number"
+            min={0}
+            value={totalBudget}
+            onChange={(e) => setTotalBudget(e.target.value)}
+          />
+          <button
+            className="save-button"
+            onClick={() => {
+              setSavedTotal(totalBudget);
+              setTotalBudget(0);
+            }}
+          >
+            Save
+          </button>
+        </div>
+        <h3>Your budget is ${savedTotal}</h3>
       </header>
       <main className="body">
         <div className="budget-container">
           <BudgetCard entry={entry} setEntry={setEntry} />
         </div>
         <div className="entry-container">
-          <AddEntry setEntry={setEntry} />
+          <AddEntry
+            setEntry={setEntry}
+            savedTotal={savedTotal}
+            setSavedTotal={setSavedTotal}
+          />
         </div>
       </main>
     </div>
