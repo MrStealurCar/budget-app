7ea884cc917f0c16d7f4d9daf3fa1a9ddce5fec5
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-06-03 12:21:52 -0400

    feat(postgres): create new Client instance and initiate connection between node and postgres

--- api/postgres.js
+++ api/postgres.js
@@ -1,1 +1,12 @@
-const pg = require("pg");
+const { Client } = require("pg");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const db = new Client({
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  user: process.env.PGUSER,
+  database: process.env.PGDATABASE,
+});
+
+db.connect();
