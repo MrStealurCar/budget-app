51e521f13271b3cd2d823e68fcfa78dec6e86379
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-06-09 11:15:30 -0400

    feat(api): create function that fetches total budget amount from GET request

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -8,4 +8,14 @@ const fetchEnvelopes = async () => {
   }
 };
 
+export const fetchBudget = async () => {
+  try {
+    const budgetResponse = await fetch("http://localhost:3005/total-budget");
+    const data = await budgetResponse.json();
+    return data;
+  } catch (error) {
+    console.error(`Error getting total budget: ${error}`);
+  }
+};
+
 export default fetchEnvelopes;
