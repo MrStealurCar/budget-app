b62bec18a9b98ea07c761a4c9097ef3d912146ec
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-06-10 12:13:48 -0400

    chore(db): delete mockEnvelopes from project after SQL refactor

--- api/db.js
+++
@@ -1,7 +0,0 @@
-const mockEnvelopes = [
-  { id: 1, title: "Groceries", budget: 300 },
-  { id: 2, title: "Rent", budget: 1200 },
-  { id: 3, title: "Entertainment", budget: 150 },
-];
-
-module.exports = { mockEnvelopes };
