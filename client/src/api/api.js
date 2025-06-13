64ac794cf11b6293e63c3dd40300f74b575867fb
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-06-13 11:15:14 -0400

    refactor(api): update fetch routes to deployed api

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,8 @@
+const API_URL = "https://budget-app-tkh2.onrender.com";
+
 const fetchEnvelopes = async () => {
   try {
-    const envelopeResponse = await fetch("http://localhost:3005/envelopes");
+    const envelopeResponse = await fetch(`${API_URL}/envelopes`);
     const data = await envelopeResponse.json();
     return data;
   } catch (error) {
@@ -10,7 +12,7 @@ const fetchEnvelopes = async () => {
 
 export const fetchBudget = async () => {
   try {
-    const budgetResponse = await fetch("http://localhost:3005/total-budget");
+    const budgetResponse = await fetch(`${API_URL}/total-budget`);
     const data = await budgetResponse.json();
     return data;
   } catch (error) {
@@ -20,19 +22,18 @@ export const fetchBudget = async () => {
 
 export const fetchTotalBudget = async (total_budget) => {
   try {
-    const budgetResponse = await fetch(
-      "http://localhost:3005/total-budget/total_budget",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ total_budget }),
-      }
-    );
+    const budgetResponse = await fetch(`${API_URL}/total-budget/total_budget`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ total_budget }),
+    });
     const data = await budgetResponse.json();
     return data;
-  } catch (error) {}
+  } catch (error) {
+    console.error(`Error updating total budget: ${error}`);
+  }
 };
 
 export default fetchEnvelopes;
