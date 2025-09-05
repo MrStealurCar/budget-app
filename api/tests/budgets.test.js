44a80e7ea671b592c1d94a3108f2561e671cb5fa
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-09-05 12:54:32 -0400

    test(budgets.test): add test for setting positive budget

---
+++ api/tests/budgets.test.js
@@ -0,0 +1,15 @@
+const assert = require("assert");
+const request = require("supertest");
+const app = require("../server");
+describe("Budget Route", () => {
+  it("expects a positive number as a total budget", async () => {
+    // Setup
+    const testBudget = 100;
+    // Exercise
+    const response = await request(app) // tells Supertest which Express app to hit.
+      .post("/total-budget/total_budget")
+      .send({ budget: testBudget });
+    // Verify
+    assert.equal(response.status, 201);
+  });
+});
