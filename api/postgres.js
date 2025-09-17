f1cd52d602079ee7d09075665e0490504f0cd7b0
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-09-17 13:15:12 -0400

    feat(postgres): use hosted database instead of local Postgres

--- api/postgres.js
+++ api/postgres.js
@@ -3,13 +3,19 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const db = new Client({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  user: process.env.PGUSER,
-  database: process.env.PGDATABASE,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-db.connect();
+db.connect()
+  .then(() => {
+    console.log("Connected to Neon database");
+  })
+  .catch((err) => {
+    console.error("Connection error", err.stack);
+  });
 
 const getAllEntries = async () => {
   try {
