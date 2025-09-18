0e0466063f74ae14b28fd7b4380ad8016b57bd45
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-09-18 09:58:22 -0400

    feat(server): allow front-end to be served in production

--- api/server.js
+++ api/server.js
@@ -3,14 +3,21 @@ const app = express();
 const PORT = process.env.PORT || 3005;
 const { envelopeRouter, totalBudgetRouter } = require("./routes/envelopes");
 const cors = require("cors");
+const path = require("path"); // Allows frontend to be served in production
 
 app.use(express.json()); //Middleware to parse incoming requests
 app.use(cors());
+
 // Registers routes for /envelopes
 app.use("/envelopes", envelopeRouter);
-
 app.use("/total-budget", totalBudgetRouter);
 
+app.use(express.static(path.join(__dirname, "../client/build"))); // Serve static files from the React app
+
+// All other GET requests not handled before will return the React app
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
