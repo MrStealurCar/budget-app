1b9784e430abcfae8d157b60fe9e148ba9d053ca
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-10-06 14:09:15 -0400

    refactor(helpers): update welcome message helper to reflect time of day

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,16 +1,17 @@
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "../firebase";
 
-export const getRandomWelcomeMessage = () => {
-  const welcomeMessages = [
-    "Hello there,",
-    "Good to see you,",
-    "Hi! Let's manage your budget,",
-    "Welcome! Let's get started,",
-  ];
-
-  const randomMsg = Math.floor(Math.random() * welcomeMessages.length);
-  return welcomeMessages[randomMsg];
+export const getWelcomeMessage = () => {
+  const welcomeMessages = [];
+  const getTime = new Date();
+  if (getTime.getHours() < 11) {
+    welcomeMessages.push("Good morning,");
+  } else if (getTime.getHours() < 18) {
+    welcomeMessages.push("Good afternoon,");
+  } else {
+    welcomeMessages.push("Good evening,");
+  }
+  return welcomeMessages[0];
 };
 
 export const getFirstName = (name) => {
