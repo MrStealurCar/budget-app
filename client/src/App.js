2b83c62398b2da9b577e8eed1dcbfd7426bdcc59
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-10-06 12:22:07 -0400

    refactor(App): move authentication and user state from ProfileMenu component

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import "./App.css";
 import { useState, useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
 import BudgetCard from "./components/BudgetCard/BudgetCard";
 import AddEntry from "./components/AddEntry/AddEntry";
 import ProfileMenu from "./components/ProfileMenu/ProfileMenu";
+import SignIn from "./components/SignIn/SignIn";
+import { handleSignIn } from "./utils/helpers";
 import { fetchBudget, fetchTotalBudget } from "./api/api";
 
 function App() {
   const [entry, setEntry] = useState([]);
   const [totalBudget, setTotalBudget] = useState("");
   const [savedTotal, setSavedTotal] = useState("");
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     const getBudget = async () => {
@@ -18,63 +23,81 @@ function App() {
     getBudget();
   }, [setSavedTotal]);
 
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <ProfileMenu />
-        <h1 className="main-title">Aura Finance</h1>
-        <div>
-          <div>
-            <div className="budget-field">
-              <h4>{savedTotal > 0 ? "" : "Enter your total budget"}</h4>
-              <input
-                className="new-budget"
-                placeholder="Total Budget"
-                type="number"
-                min={0}
-                value={totalBudget}
-                onChange={(e) => setTotalBudget(e.target.value)}
-              />
-              <button
-                className="save-button"
-                onClick={async () => {
-                  await fetchTotalBudget(totalBudget);
-                  // Allows 0 to be set as a total budget
-                  if (totalBudget !== undefined && totalBudget !== null) {
-                    setSavedTotal(totalBudget);
-                    setTotalBudget("");
-                  } else {
-                    alert("Amount cannot be a negative number.");
-                  }
-                }}
-              >
-                Save
-              </button>
-            </div>
-            <h3>
-              {savedTotal > 0 ? `Your budget is $${Number(savedTotal)}` : ""}
-            </h3>
-          </div>
-        </div>
-      </header>
+      <h1 className="main-title">Aura Finance</h1>
+      {user ? (
+        <>
+          <header className="App-header">
+            <ProfileMenu user={user} />
 
-      <main className="body">
-        <div className="budget-container">
-          <BudgetCard
-            entry={entry}
-            setEntry={setEntry}
-            savedTotal={savedTotal}
-            setSavedTotal={setSavedTotal}
-          />
-        </div>
-        <div className="entry-container">
-          <AddEntry
-            setEntry={setEntry}
-            savedTotal={savedTotal}
-            setSavedTotal={setSavedTotal}
-          />
-        </div>
-      </main>
+            <div>
+              <div>
+                <div className="budget-field">
+                  <h4>{savedTotal > 0 ? "" : "Enter your total budget"}</h4>
+                  <input
+                    className="new-budget"
+                    placeholder="Total Budget"
+                    type="number"
+                    min={0}
+                    value={totalBudget}
+                    onChange={(e) => setTotalBudget(e.target.value)}
+                  />
+                  <button
+                    className="save-button"
+                    onClick={async () => {
+                      await fetchTotalBudget(totalBudget);
+                      // Allows 0 to be set as a total budget
+                      if (totalBudget !== undefined && totalBudget !== null) {
+                        setSavedTotal(totalBudget);
+                        setTotalBudget("");
+                      } else {
+                        alert("Amount cannot be a negative number.");
+                      }
+                    }}
+                  >
+                    Save
+                  </button>
+                </div>
+                <h3>
+                  {savedTotal > 0
+                    ? `Your budget is $${Number(savedTotal)}`
+                    : ""}
+                </h3>
+              </div>
+            </div>
+          </header>
+
+          <main className="body">
+            <div className="budget-container">
+              <BudgetCard
+                entry={entry}
+                setEntry={setEntry}
+                savedTotal={savedTotal}
+                setSavedTotal={setSavedTotal}
+                user={user}
+              />
+            </div>
+            <div className="entry-container">
+              <AddEntry
+                setEntry={setEntry}
+                savedTotal={savedTotal}
+                setSavedTotal={setSavedTotal}
+                user={user}
+              />
+            </div>
+          </main>
+        </>
+      ) : (
+        <SignIn user={user} handleSignIn={handleSignIn} />
+      )}
     </div>
   );
 }
--- client/src/components/ProfileMenu/ProfileMenu.js
+++ client/src/components/ProfileMenu/ProfileMenu.js
@@ -1,19 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./ProfileMenu.css";
 import { FaUser } from "react-icons/fa";
-import { signInWithGoogle, logout, auth } from "../../firebase";
-import { onAuthStateChanged } from "firebase/auth";
+import { logout } from "../../firebase";
 import { getInitials } from "../../utils/helpers";
-function ProfileMenu() {
-  const [user, setUser] = useState(null);
+function ProfileMenu({ user }) {
   const [open, setOpen] = useState(false);
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-    });
-    return () => unsubscribe();
-  }, []);
   return (
     <div>
       {user ? (
@@ -37,7 +29,6 @@ function ProfileMenu() {
         <div className="sign-in-container">
           <FaUser
             size={24}
-            onClick={signInWithGoogle}
             className="sign-in-icon"
             title="Sign in with Google"
           />
