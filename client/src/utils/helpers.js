b5880913f564a9eba10368b64ef8279ca92ec3e9
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-10-06 12:17:05 -0400

    feat(helpers): create and export sign in function

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,6 +1,19 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../firebase";
+
 export const getInitials = (name) => {
   if (!name) return "";
   const names = name.split(" ");
   const initials = names.map((name) => name.charAt(0).toUpperCase());
   return initials.slice(0, 2).join("");
+};
+
+export const handleSignIn = async (onSuccess) => {
+  try {
+    const provider = new GoogleAuthProvider();
+    const result = await signInWithPopup(auth, provider);
+    onSuccess(result.user);
+  } catch (error) {
+    console.error("Sign-in error:", error);
+  }
 };
