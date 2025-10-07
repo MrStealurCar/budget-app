25d62866b4e56ccc31c0af8bf2538e65ff4114e5
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-10-07 13:17:43 -0400

    refactor(SignIn): import handleSignIn directly rather than via props

--- client/src/components/SignIn/SignIn.js
+++ client/src/components/SignIn/SignIn.js
@@ -1,10 +1,10 @@
 import "./SignIn.css";
-
-export default function SignIn({ handleSignIn }) {
+import { handleSignIn } from "../../utils/helpers";
+export default function SignIn() {
   return (
     <div className="sign-in-container">
       <h2 className="sign-in-title">Welcome to Aura Finance</h2>
-      <p>Please sign in with Google to continue</p>
+      <p className="prompt">Please sign in with Google to continue</p>
       <button onClick={handleSignIn} className="sign-in-button">
         Sign in
       </button>
