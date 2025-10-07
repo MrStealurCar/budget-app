aa40e7be5f5066fd945683240b9f5b9683e1302c
Jacob Rodriguez <jacob_rod1231@outlook.com>
2025-10-07 13:21:49 -0400

    feat(api): update fetch routes to include user's id

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -8,27 +8,29 @@ const fetchEnvelopes = async () => {
   }
 };
 
-export const fetchBudget = async () => {
+export const fetchBudget = async (user) => {
   try {
-    const budgetResponse = await fetch(`/total-budget`);
+    const budgetResponse = await fetch(`/total-budget`, {
+      headers: { user_id: user.uid },
+    });
     const data = await budgetResponse.json();
-    return data;
+    return data.total_budget;
   } catch (error) {
     console.error(`Error getting total budget: ${error}`);
   }
 };
 
-export const fetchTotalBudget = async (total_budget) => {
+export const fetchTotalBudget = async (total_budget, user) => {
   try {
     const budgetResponse = await fetch(`/total-budget/total_budget`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ total_budget }),
+      body: JSON.stringify({ total_budget, user_id: user.uid }),
     });
     const data = await budgetResponse.json();
-    return data;
+    return data.total_budget;
   } catch (error) {
     console.error(`Error fetching budget data: ${error}`);
   }
